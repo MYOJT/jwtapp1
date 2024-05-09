7a7efcc973affafc507dab68ab8d8da42652d7e7
MYOJT <114838664+MYOJT@users.noreply.github.com>
2024-05-09 21:14:49 +0900

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,3 +1,19 @@
+-- DDL準備
+-- Redshiftのクエリエディタで実行可能なDDLスクリプト
+CREATE TABLE example_table (
+    id INT,
+    content VARCHAR(100),
+    create_date VARCHAR(30)
+);
+
+-- Auroraのクエリエディタで実行可能なDDLスクリプト
+CREATE TABLE temp_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(100),
+    create_date TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6)
+);
+
+-- テストデータ準備(Auroraのクエリエディタで実行する）
 -- ループの回数を指定
 SET @i = 1;
 SET @max = 10000; -- 挿入するレコードの数
