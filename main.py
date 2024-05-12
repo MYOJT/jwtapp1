80832643a8a1a2a6616feff1ba651f8eb19106a7
MYOJT <114838664+MYOJT@users.noreply.github.com>
2024-05-13 07:55:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,40 @@ S3_SECRET_KEY = 'YOUR_S3_SECRET_KEY'
 s3 = boto3.client('s3',
                   aws_access_key_id=S3_ACCESS_KEY,
                   aws_secret_access_key=S3_SECRET_KEY)
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # SQSからメッセージを受信
+    sqs = boto3.client('sqs')
+    queue_url = 'YOUR_QUEUE_URL'  # SQSのキューのURLを指定してください
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,
+        VisibilityTimeout=30,
+        WaitTimeSeconds=20
+    )
+    
+    messages = response.get('Messages', [])
+    
+    for message in messages:
+        # メッセージの本文を取得
+        body = message['Body']
+        
+        # メッセージを処理するための処理を記述
+        # 例えば、メッセージを解析して必要な処理を行います
+        
+        # 処理が完了したら、メッセージを削除
+        receipt_handle = message['ReceiptHandle']
+        sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('SQSからのメッセージを処理しました')
+    }
 
 @app.route('/')
 def index():
